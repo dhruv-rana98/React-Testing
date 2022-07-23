4728ee0987a7ffd60fa65c81644beea883fc4cf4
Dhruv Rana <dhruvrana0000@gmail.com>
2022-07-23 14:18:00 +0530

    Creating the first test

---
+++ src/components/Greetings.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greetings";
+test("renders hello world as a text", () => {
+  //Use AAA's for testing
+  // A Arrange the code and render the component
+  render(<Greeting />); // rendering the component
+  //A act - simulate the functionality
+  //... for this case nothing as there is not action taking place
+
+  //A Assert the results - verify the tests with the coming and the expected results
+  const helloWorldElement = screen.getByText("Hello World!", { exact: false });
+  expect(helloWorldElement).toBeInTheDocument();
+});
