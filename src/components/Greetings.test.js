75db2ba63b5a2b8f81f66363a8c87a0e3b0a382a
Dhruv Rana <dhruvrana0000@gmail.com>
2022-07-23 14:40:56 +0530

    Writing a test for a complex component with elements using state and click of button

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greetings";
 
 //Test Suite
@@ -16,5 +17,37 @@ describe("Greeting componnt test", () => {
       exact: false,
     });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders button and paragraph text", () => {
+    render(<Greeting />);
+    const button = screen.getByText("Change Text!");
+    const paragraph = screen.getByText("It's good to see you.");
+    expect(button).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+  });
+  test("render changed if the button was clicked", () => {
+    //Arrange
+    render(<Greeting />);
+    //Act
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    //Assert
+    const outputElement = screen.getByText("Changed");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    //Assert
+    const outputElement = screen.queryByText("It's good to see you."); //As getByText will throw error if the element is not found
+    expect(outputElement).toBeNull();
   });
 });
